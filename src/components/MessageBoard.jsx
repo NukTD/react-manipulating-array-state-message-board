c0dc4dea5384086f7ed6b25fefaad6bbec8cba46
TheYami <luffy90san@gmail.com>
2024-10-24 13:18:26 +0700

    feat: Add form controller

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
+
 function MessageBoard() {
+
+  const [messageBox, setMessageBox] = useState([]);
+  const [messageText,setMessageText] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const newMessageBox = [...messageBox,messageText];
+    setMessageBox(newMessageBox);
+    setMessageText('')
+  }
+
+  const deleteMessage = (index) => {
+    const newMessageBox = [...messageBox]
+    newMessageBox.splice(index,1);
+    setMessageBox(newMessageBox)
+  }
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
-        <label>
-          <input
-            id="message-text"
-            name="message-text"
-            type="text"
-            placeholder="Enter message here"
-          />
-        </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+      <form onSubmit={handleSubmit}>
+        <div class="message-input-container">
+          <label>
+            <input
+              id="message-text"
+              name="message-text"
+              type="text"
+              placeholder="Enter message here"
+              value={messageText}
+              onChange={(e) => setMessageText(e.target.value)}
+            />
+          </label>
+          <button className="submit-message-button" type="submit">
+            Submit
+          </button>
         </div>
+      </form>
+
+      <div class="board">
+        {messageBox.map((message, index) => (
+          <div className="message" key={index}>
+            <h1>{message}</h1>
+            <button className="delete-button" onClick={()=>{deleteMessage(index)}}>x</button>
+          </div>
+        ))}
       </div>
     </div>
   );
